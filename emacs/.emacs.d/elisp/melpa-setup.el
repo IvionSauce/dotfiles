95f6f43b85ea14402329f84145a0419fae5fbe13
Ivion <ivion@xs4all.nl>
2019-08-17 17:06:42 +0200

    Add avy package and reformat

--- emacs/.emacs.d/elisp/melpa-setup.el
+++ emacs/.emacs.d/elisp/melpa-setup.el
@@ -13,27 +13,34 @@
 (setq use-package-always-ensure t)
 
 (use-package ido-completing-read+
-	     :config
-	     (ido-ubiquitous-mode 1))
+  :config
+  (ido-ubiquitous-mode 1))
 
 (use-package smex
-	     :bind
-	     (("M-x" . smex)
-	      ("M-X" . smex-major-mode-commands))
-	     :config
-	     (smex-initialize))
+  :bind
+  (("M-x" . smex)
+   ("M-X" . smex-major-mode-commands))
+  :config
+  (smex-initialize))
 
 (use-package ibuffer-vc
-	     :config
-	     ;; Extends and configures ibuffer(-vc)
-	     (require 'gk-ibuffer))
+  :config
+  ;; Extends and configures ibuffer(-vc)
+  (require 'gk-ibuffer))
 
 (use-package expand-region
-	     :bind
-	     ("C-=" . er/expand-region))
+  :bind
+  ("C-=" . er/expand-region))
+
+(use-package avy
+  :bind
+  (("M-g M-g" . avy-goto-word-1)
+   ("M-g g" . avy-goto-line)
+   ("M-g SPC" . avy-goto-char-timer)
+   ("M-g M-SPC" . avy-goto-char-timer)))
 
 (use-package markdown-mode
-	     :hook
-	     (markdown-mode . visual-line-mode))
+  :hook
+  (markdown-mode . visual-line-mode))
 
 (provide 'melpa-setup)
